fa54ee8b1b0fb4d5b27233978e007cf574842cb5
Stephen Lin <Stephen Lin>
2017-03-23 00:46:27 +0800

    Initial commit

---
+++ Myfirst.cpp
@@ -0,0 +1,15 @@
+//myfirst.cpp -- displays a message
+// Created by Stephen on 2017-03-22.
+//
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    cin.get();
+    return 0;
+}
